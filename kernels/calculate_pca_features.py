4e6fe67deb000e9c0de80078b42e73d830e12ed8
yongming <yongming_shi@163.com>
2021-01-21 20:31:22 +0800

    add extract_features modelu and switch vscode to pycharm

---
+++ kernels/calculate_pca_features.py
@@ -0,0 +1,5 @@
+#from tools.extract_features import extract_features_array
+import os
+import sys
+
+from tools.extract_features import extract_features_array
--- tools/voc_data_processing.py
+++ tools/voc_data_processing.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 from kernels.calculate_voc_wh_xy import calculate_wh_xy
 
-def generate_xml_and_image_list(txt_path=None, xml_folder=None):
+def generate_xml_and_image_list(txt_path=None, xml_folder=None, image_folder=None):
     f = open(txt_path)
     xml_file_list = []
     image_list = []
@@ -17,9 +17,10 @@ def generate_xml_and_image_list(txt_path=None, xml_folder=None):
         xml_file_list.append(xml_filename)
         
         #generate image filename list
-        xml_tree = xml.dom.minidom.parse(xml_filename)
-        rootNode = xml_tree.documentElement
-        image_name = rootNode.getElementsByTagName("filename")[0].firstChild.data
+        # xml_tree = xml.dom.minidom.parse(xml_filename)
+        # rootNode = xml_tree.documentElement
+        # image_name = rootNode.getElementsByTagName("filename")[0].firstChild.data
+        image_name = image_folder + line[:-1] + '.jpg'
         image_list.append(image_name)
     return xml_file_list, image_list
 
@@ -40,7 +41,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser() 
     parser.add_argument('--txt_path', '-t', default=None, help='path of the txt file')
     parser.add_argument('--xml_folder', '-x', default=None, help='folder of the xml file')
+    parser.add_argument('--image_folder', '-i', default=None, help='folder of images')
     args = parser.parse_args()
-    xml_list, image_list = generate_xml_and_image_list(args.txt_path, args.xml_folder)
+    xml_list, image_list = generate_xml_and_image_list(args.txt_path, args.xml_folder, args.image_folder)
     wh_list, _ = generate_wh_xyminmax_list(args.txt_path, args.xml_folder)
     # print(image_list)
